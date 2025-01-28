48749ca55d1d46fe4a7c884597fe40ec94023f70
Gio Victor Rodriguez <giorodriguez021@gmail.com>
2025-01-29 00:32:52 +0800

    Added metadata in head tag for search engine

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,14 @@ export default function Document() {
         <Html lang="en">
             <Head>
                 <title>Timeless Treasures PH</title>
+                <meta name="description" content="Memories are Timeless Treasures of the Heart" />
+                <meta name="keywords" content="timelesstreasures, timelesstreasuresph, timeless, treasures, timelessph, treasuresph, craft, rentals, souvenirs, styling, weddings, events, personalized" />
+                <meta property="og:title" content="Timeless Treasures PH" />
+                <meta property="og:description" content="Memories are Timeless Treasures of the Heart" />
+                <meta property="og:type" content="website" />
+                <meta property="og:image" content="/images/header-logo.jpg" />
+                <meta property="og:site_name" content="Timeless Treasures PH" />
+                <link rel="icon" href="/favicon.ico" />
             </Head>
             <body>
                 <Main />
